598979e5ab3a212b4745fa01cb077034efaaa5be
sbociot <stevebocicot@icloud.com>
2016-11-08 10:52:05 -0500

    groseiller

--- script/main.js
+++ script/main.js
@@ -1,14 +1,3 @@
-//"use strict";
-////Rechercher un elelment singulier par son Id (html unique)
-////Placer dans la variable le resultat
-//var table_produit = document.getElementById('produits_promo');
-//console.log("Element : " , table_produit);
-//
-//var row_items = table_produit.getElementsByTagName('tr');
-//console.log("Element :" , row_items);
-//console.log("Nb d'items :" ,row_items.length);
-// document.getElementById('promo').innerHTML
-//var element = document.getElementById('nouvelles').textContent;
-// document.getElementById('nouvelles').innerHTML = element;
-//document.queryselector('#nouvelles h2').innerHTMl = "Nos nouvelles";
-document.getElementById('nouvelles').innerHTML = 'Voici nos nouvellles';
+"use strict";
+//Methode query selector
+document.querySelector('#nouvelles h2:first-of-type').textContent = "Nouvelles Fraiches";
