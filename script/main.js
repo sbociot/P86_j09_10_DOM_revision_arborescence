ee511d6ef845ab9750e92f0926e1c9cd032756a3
sbociot <stevebocicot@icloud.com>
2016-11-08 00:35:37 -0500

    second one

---
+++ script/main.js
@@ -0,0 +1,12 @@
+//"use strict";
+////Rechercher un elelment singulier par son Id (html unique)
+////Placer dans la variable le resultat
+//var table_produit = document.getElementById('produits_promo');
+//console.log("Element : " , table_produit);
+//
+//var row_items = table_produit.getElementsByTagName('tr');
+//console.log("Element :" , row_items);
+//console.log("Nb d'items :" ,row_items.length);
+// document.getElementById('promo').innerHTML
+//var element = document.getElementById('nouvelles').textContent;
+// document.getElementById('nouvelles').innerHTML = element;
